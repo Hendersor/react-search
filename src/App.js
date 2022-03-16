a1019f81b4b85d2ffaa36fc5e78e877b29b9e21a
Francisco <montejof1998@gmail.com>
2022-03-15 19:31:07 -0700

    Design ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
+import React from "react";
+import Item from "./Item";
 import "./App.scss";
+import "./Item";
+const { faker } = require("@faker-js/faker");
+
+const fakeUsers = [
+  {
+    name: faker.name.firstName(),
+    country: faker.address.country(),
+    image: faker.image.people(),
+    number: faker.random.number(),
+  },
+  {
+    name: faker.name.firstName(),
+    country: faker.address.country(),
+    image: faker.image.people(),
+    number: faker.random.number(),
+  },
+  {
+    name: faker.name.firstName(),
+    country: faker.address.country(),
+    image: faker.image.people(),
+    number: faker.random.number(),
+  },
+  {
+    name: faker.name.firstName(),
+    country: faker.address.country(),
+    image: faker.image.people(),
+    number: faker.random.number(),
+  },
+];
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <section className="inputContainer">
         <input className="input" /> <button> Search</button>
       </section>
 
-      <section className="itemsContainer"></section>
+      <section className="itemsContainer">
+        {fakeUsers.map((user) => (
+          <Item
+            key={user.number}
+            name={user.name}
+            country={user.country}
+            image={user.image}
+          />
+        ))}
+      </section>
     </div>
   );
 }
